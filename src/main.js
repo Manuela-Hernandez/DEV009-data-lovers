cc9d610e7e12fb7f7f922f59c95fc8d0a0859802
LauraJoya19 <lalujoya@gmail.com>
2023-06-28 19:22:14 -0500

    animacion imagen hover

--- src/main.js
+++ src/main.js
@@ -6,22 +6,22 @@ console.log(example, data);
 
 // manipulacion del DOM
 
-function dibujarPersonajes () {
-    data["got"].forEach(element => {
-        const newDiv = document.createElement("div");
-        newDiv.classList.add("personaje");
-        const container = document.getElementById("container");
-        container.appendChild(newDiv);
-        //newDiv.textContent = element["Daenerys Targaryen"];
-        const newContent = document.createTextNode("p");
-        newContent.textContent = element["fullName"];
-        newDiv.appendChild(newContent);
-        const newImg = document.createElement ("img");
-        newImg.src = element["imageUrl"];
-        newDiv.appendChild(newImg);
+/* //function dibujarPersonajes () {
+  data["got"].forEach(element => { 
+    const newDiv = document.createElement("div");
+    newDiv.classList.add("personaje");
+    const container = document.getElementById("container");
+    container.appendChild(newDiv);
+    //newDiv.textContent = element["Daenerys Targaryen"];
+    const newContent = document.createTextNode("p");
+    newContent.textContent = element["fullName"];
+    newDiv.appendChild(newContent);
+    const newImg = document.createElement ("img");
+    newImg.src = element["imageUrl"];
+    newDiv.appendChild(newImg);
 
-        console.log (element["imageUrl"]);
+    console.log (element["imageUrl"]);
        
-    });
+  });
 }
-dibujarPersonajes()
+dibujarPersonajes() */
